1be782a35ff538481359131a9a00fbe935f89271
AbolfazlSafari06 <abolfazlsafari4729@gmail.com>
2021-11-04 10:47:12 +0330

    ایجاد پروژه

---
+++ src/layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Layout() {
+    return (
+        <div>
+            <div>
+                Header
+            </div>
+            <div>
+                Main
+            </div>
+            <div>
+                Footer
+            </div>
+        </div>
+    )
+}
+
+export default Layout
